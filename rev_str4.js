0d4b28ee5338a9a8a9b3f3e64487feaf8890e192
Davor Kandic <markojank727@gmail.com>
2019-12-21 00:36:47 +0100

    reverse_string_es6_using_spread_operator

---
+++ rev_str4.js
@@ -0,0 +1,8 @@
+// reverse string ecmascript6 way, usung the spread operator
+
+
+const rev_str4 = str => [...str].reverse().join('');
+
+stringy = "Gaudeamus igitur iuvenes dum summus";
+
+console.log(rev_str4(stringy));
